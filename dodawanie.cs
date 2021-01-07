db79c0d20df109f1683cd3069982b62820f13b8c
DiamondHoe <desineverbum@gmail.com>
2021-01-07 20:30:49 +0100

    AAAAAAAAAAA

--- dodawanie.cs
+++ dodawanie.cs
@@ -3,6 +3,10 @@
 break;
 =======
 ﻿case "d":
+<<<<<<< HEAD
                     Consooole.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie Norbert szefie proszę mnie nie zwalniac z pracy
+=======
+                    Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie Norbert szefie proszę mnie nie zwalniac z pracyaaaaaaaaaaaaa
+>>>>>>> 908689c (AAAAAAAAAAA)
 break;
 >>>>>>> a9b90f2 (Ou je)
