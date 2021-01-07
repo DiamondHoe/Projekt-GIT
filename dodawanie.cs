e220fc38df456432cac3313af7cd8122a7946033
DiamondHoe <desineverbum@gmail.com>
2021-01-07 20:37:07 +0100

    Ou je

--- dodawanie.cs
+++ dodawanie.cs
@@ -1,2 +1,8 @@
+<<<<<<< HEAD
 ﻿                    Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie Norbert szefie proszę mnie nie zwalniac z pracy
 break;
+=======
+﻿case "d":
+                    Consooole.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie Norbert szefie proszę mnie nie zwalniac z pracy
+break;
+>>>>>>> a9b90f2 (Ou je)
