74861981661be316a1c810577ffdc0e76ecdaa82
DiamondHoe <desineverbum@gmail.com>
2021-01-07 20:14:53 +0100

    Dodanie prawdy o Pawle

--- dodawanie.cs
+++ dodawanie.cs
@@ -1,3 +1,3 @@
 ﻿case "d":
-                    Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie
+                    Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie Paweł jest za arką gdynią
 break;
