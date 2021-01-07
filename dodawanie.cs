dc36e3b89099577d3c749cce3a7a64e59e88e12e
DiamondHoe <desineverbum@gmail.com>
2021-01-07 20:34:28 +0100

    UUUUUU

--- dodawanie.cs
+++ dodawanie.cs
@@ -5,6 +5,7 @@ break;
 ﻿case "d":
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                     Consooole.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie Norbert szefie proszę mnie nie zwalniac z pracy
 =======
                     Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie Norbert szefie proszę mnie nie zwalniac z pracyaaaaaaaaaaaaa
@@ -12,5 +13,8 @@ break;
 =======
                     Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcsssssja wykonująca  Norbert szefie proszę mniqsfdasdase nie zwalniac z pracyaaaaaaaaaaaaa
 >>>>>>> 945761d (YYYYY)
+=======
+                    Cosnolesadale.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcsssssja wykonująca  Norbert szefie proszę mniqsfdasdase nie zwalniac z pracyaaaaaaaaaaaaa
+>>>>>>> 9f86ac9 (UUUUUU)
 break;
 >>>>>>> a9b90f2 (Ou je)
