f2046a295604592dd7061230760feba74a795010
Pawlonix <pawelek.2611@wp.pl>
2021-01-07 19:40:56 +0100

    Poprawa bledu ortograficznego

--- dodawanie.cs
+++ dodawanie.cs
@@ -1,3 +1,3 @@
 ﻿case "d":
-                    Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dielenie
+                    Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie
 break;
