9f86ac9a7c2f040ba5342906134fd0b7cf7ad151
DiamondHoe <desineverbum@gmail.com>
2021-01-07 20:34:28 +0100

    UUUUUU

--- dodawanie.cs
+++ dodawanie.cs
@@ -1,3 +1,3 @@
 ﻿case "d":
-                    Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcsssssja wykonująca  Norbert szefie proszę mniqsfdasdase nie zwalniac z pracyaaaaaaaaaaaaa
+                    Cosnolesadale.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcsssssja wykonująca  Norbert szefie proszę mniqsfdasdase nie zwalniac z pracyaaaaaaaaaaaaa
 break;
