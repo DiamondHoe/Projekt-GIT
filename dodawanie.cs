1ef8ad48299b8686e85a026786fbb5260a2805a2
Pawlonix <pawelek.2611@wp.pl>
2021-01-07 20:15:13 +0100

    Arka Gdynia

--- dodawanie.cs
+++ dodawanie.cs
@@ -1,3 +1,2 @@
-﻿case "d":
-                    Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie Paweł jest za arką gdynią
+﻿                    Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie Norbert szefie proszę mnie nie zwalniac z pracy
 break;
