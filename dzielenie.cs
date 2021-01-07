b1bc2e6afa0a5ad04213090bdf40230922a24891
mikrus000 <mikrus000@utp.edu.pl>
2021-01-07 19:32:32 +0100

    dzielenie

--- dzielenie.cs
+++ dzielenie.cs
@@ -1,3 +1,3 @@
-case "o":
-    Console.WriteLine($"Twoj wynik: {num1} - {num2} = " + (num1 - num2));
+case "dz":
+    Console.WriteLine($"Twoj wynik: {num1} / {num2} = " + (num1 / num2));
     break;
