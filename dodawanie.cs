908689cc971760695c7814cdf8957acb1bb6cd38
DiamondHoe <desineverbum@gmail.com>
2021-01-07 20:30:49 +0100

    AAAAAAAAAAA

--- dodawanie.cs
+++ dodawanie.cs
@@ -1,3 +1,3 @@
 ﻿case "d":
-                    Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie Norbert szefie proszę mnie nie zwalniac z pracy
+                    Console.WriteLine($"Twój wynik: {num1} + {num2} = " + (num1 + num2)); //Funkcja wykonująca dzielenie Norbert szefie proszę mnie nie zwalniac z pracyaaaaaaaaaaaaa
 break;
